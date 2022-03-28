53315cd150f4577ebb02a22490a574a819ee5810
Henry <uce.henrys@gmail.com>
2022-03-28 10:06:54 +0100

    added schema

---
+++ db/setup.js
@@ -0,0 +1,49 @@
+db.createCollection("users", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["username", "password"],
+            properties:{
+                username: {
+                    bsonType: "string",
+                    description: "Not a string!"
+                },
+                password: {
+                    bsonType: "string",
+                    description: "Not a string!"
+                },
+                habits : {
+                    bsonType: "object",
+                    description: "Not an object!",
+                    required: ["habitName", "schedule", "completed", "dates", "currentStreak", "longestStreak"],
+                    properties: {
+                        habitName: {
+                            bsonType: "string",
+                            description: "Not a string!"
+                        },
+                        schedule: {
+                            bsonType: "string",
+                            description: "Not a string!"
+                        },
+                        completed: {
+                            bsonType: "bool",
+                            description: "Not a bool!"
+                        },
+                        dates: {
+                            bsonType: [],
+                            description: "Not a date"
+                        },
+                        currentStreak: {
+                            bsonType: "int",
+                            description: "Not an int!"
+                        },
+                        longestStreak: {
+                            bsonType: "int",
+                            description: "Not an int!"
+                        }
+                    }
+                }
+            }
+        }
+    }
+})

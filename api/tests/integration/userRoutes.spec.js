46a540ebdd34a7a5171d17e37bb99243b6d620a8
idrisimo <idrissilva@hotmail.com>
2022-03-28 11:05:25 +0100

    adding comments for testing

---
+++ api/tests/integration/userRoutes.spec.js
@@ -0,0 +1,11 @@
+// test server running
+
+// should returning all users
+
+// should create a new user
+
+// Should have password hashed and salted.
+
+// Should not create user that already exists
+
+// Should delelete user

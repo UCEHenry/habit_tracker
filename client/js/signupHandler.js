854d671d9baa9732e9666289daf25002a2f95146
idrisimo <idrissilva@hotmail.com>
2022-03-29 11:42:31 +0100

    function for signup pretty much done just need to do event listener

--- client/js/signupHandler.js
+++ client/js/signupHandler.js
@@ -3,9 +3,22 @@ function registerNewUser(event) {
     console.log('registration form submitted')
     try{
         const userData = {
-            username: event.target[''].value,
-            password: event.target[''].value
+            username: event.target['username'].value,
+            password: event.target['password'].value
         }
-        const options = {}
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        fetch(`${API_URL}/users`, options)
+        .then(resp = resp.json())
+        .then(data => {
+            // if (response is positive) {redirect to dashboard} else {show error to user}
+        })
+    } catch (err) {
+        alert(`Account creation failed: ${err}`)
     }
 }
---
+++ client/js/urls.js
@@ -0,0 +1,10 @@
+
+// development
+const API_URL = 'http://localhost:3000'
+
+// production
+// const API_URL = ''
+
+
+
+module.exports = {API_URL};

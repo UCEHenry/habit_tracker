ba986e3ab50c4fa8b80b11d392faed40c06b6a2b
Henry <uce.henrys@gmail.com>
2022-03-29 12:32:58 +0100

    checking password matching

--- client/js/signupHandler.js
+++ client/js/signupHandler.js
@@ -3,25 +3,29 @@ const signupFormSubmit = document.getElementById('signupForm').addEventListener(
 async function registerNewUser(event) {
     event.preventDefault()
 
-    const userData = {
-        username: event.target['registerUsername'].value,
-        password: event.target['registerPassword'].value
-    }
-
-    try{
-        const options = {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: {
-                "Content-Type": "application/json"
-            }
+    if (event.target.registerPassword != event.target.registerCheckPassword) {
+        alert("Password does not match")
+    } else {
+        const userData = {
+            username: event.target['registerUsername'].value,
+            password: event.target['registerPassword'].value
         }
 
-        const response = await fetch(`${API_URL}/users`, options);
-        const data = await response.json()
+        try {
+            const options = {
+                method: 'POST',
+                body: JSON.stringify(userData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
 
-    } catch (err) {
-        alert(`Account creation failed: ${err}`)
-        console.log(`Account not created: ${err}`);
+            const response = await fetch(`${API_URL}/users`, options);
+            const data = await response.json()
+
+        } catch (err) {
+            alert(`Account creation failed: ${err}`)
+            console.log(`Account not created: ${err}`);
+        }
     }
 }

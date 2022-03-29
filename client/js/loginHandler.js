6cfe550778c4d3e35f62523f7e16d204d3e406b0
Henry <uce.henrys@gmail.com>
2022-03-29 11:41:55 +0100

    added log in functionality with test

---
+++ client/js/loginHandler.js
@@ -0,0 +1,39 @@
+const logInForm = document.querySelector(".logInForm")
+
+logInForm.addEventListener('submit', userLogIn)
+
+async function userLogIn (e) {
+    e.preventDefault()
+    console.log(e)
+
+    const userData = {
+        username: e.target.exampleInputUsername1.value,
+        password: e.target.exampleInputPassword1.value,
+    };
+    console.log(e.target)
+    try {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        };
+    
+        console.log(options.body)
+        
+        const response = await fetch(url, options);
+        let data = await response.json();
+        // if (data.err) {
+        //     throw Error(data.err);
+        // }
+        // else (
+        //     // var current = window.location.href;
+        //     // window.location.href = current.replace(/#(.*)$/, '') + '#' + id;
+        //     window.location.hash = `#${data.id}`
+        // )
+    } catch (err) {
+        alert(`Unable to Log In: ${err}`);
+        console.log(`Failed to get Log In, reason: ${err}`);
+    }
+}

2d2062c94e184b4fbedd94af6179502186717063
idrisimo <idrissilva@hotmail.com>
2022-03-29 17:36:28 +0100

    data has ids for the template

--- client/js/habitCardHandler.js
+++ client/js/habitCardHandler.js
@@ -1,15 +1,21 @@
 window.onload = event => {
     let data = {
-        
+
         habitList: [
-            { habitName: "Exercise", id:"1",  longestStreak:"1", currentStreak:"1", frequency: "daily" },
-            { habitName: "Drink Water", id:"2" },
-            { habitName: "Sleep", id:"3" }
-        ],
+            { habitName: "Exercise", longestStreak: "1", currentStreak: "1", frequency: "daily" },
+            { habitName: "Drink Water" },
+            { habitName: "Sleep" }
+        ]
     };
-    
+    let counter = 0
+    let dataWithId = {habitList:[]}
+    for (item of data['habitList']) {
+        item['id'] = counter
+        counter ++
+        dataWithId['habitList'].push(item)
+    }
     let template = Handlebars.compile(document.querySelector('#template').innerHTML);
-    let filled = template(data, {
+    let filled = template(dataWithId, {
         noEscape: true
     })
     document.querySelector('#habitsSection').innerHTML = filled;

c94334cf05ad11c9f823c841f87b5613222a1b60
idrisimo <idrissilva@hotmail.com>
2022-03-29 11:05:47 +0100

    added tests for login and signup

--- client/tests/index.test.js
+++ client/tests/index.test.js
@@ -6,39 +6,84 @@ global.fetch = require('jest-fetch-mock');
 let app;
 
 describe("Checks JS files", () => {
-    describe("loginHandler.js", () => {
-        beforeEach(()=>{
-            app = require("../js/loginHandler")
-            document.documentElement.innerHTML = html.toString();
+    describe("User account interaction", () => {
+        let username = "testUser"
+        let password = "test123"
+        describe("loginHandler.js", () => {
+
+            beforeEach(() => {
+                app = require("../js/loginHandler")
+                document.documentElement.innerHTML = html.toString();
+            })
+            afterEach(() => {
+                fetch.resetMocks();
+            })
+            // Should send login data to backend
+            it("Should send login data to backend", () => {
+                const fakeSubmitEvent = {
+                    preventDefault: jest.fn(),
+                    target: [{ value: username }, { value: password }]
+                }
+                app.submitLogin(fakeSubmitEvent)
+                expect(fetch.mock.calls[0][1]).toHaveProperty("method", "POST")
+                expect(fetch.mock.calls[0][1]).toHaveProperty("body", JSON.stringify({ username: username, password: password }))
+            })
+            // Should send success of submission
+            it("Should send success of submission", async () => {
+                const fakeSubmitEvent = {
+                    preventDefault: jest.fn(),
+                    target: [{ value: username }, { value: password }]
+                }
+                const response = await app.submitLogin(fakeSubmitEvent)
+                expect(response).toStrictEqual({ "status": 200 })
+            })
+            // Should send failed submission 
+            it("Should send error due to user not existsing", async () => {
+                const fakeSubmitEvent = {
+                    preventDefault: jest.fn(),
+                    target: [{ value: "tesyMcTestface" }, { value: "test1234" }]
+                }
+                const response = await app.submitLogin(fakeSubmitEvent)
+                expect(response).toStrictEqual({ "status": 404 })
+            })
         })
-        afterEach(() => {
-            fetch.resetMocks();
+        describe("signupHandler.js", () => {
+            beforeEach(() => {
+                app = require("../js/signupHandler")
+                document.documentElement.innerHTML = html.toString();
+            })
+            afterEach(() => {
+                fetch.resetMocks();
+            })
+            // Should send signup data to backend
+            it("Should send signup data to backend", () => {
+                const fakeSubmitEvent = {
+                    preventDefault: jest.fn(),
+                    target: [{ value: username }, { value: password }]
+                }
+                app.submitLogin(fakeSubmitEvent)
+                expect(fetch.mock.calls[0][1]).toHaveProperty("method", "POST")
+                expect(fetch.mock.calls[0][1]).toHaveProperty("body", JSON.stringify({ username: username, password: password }))
+            })
+            // Should send success of submission
+            it("Should send success of submission", async () => {
+                const fakeSubmitEvent = {
+                    preventDefault: jest.fn(),
+                    target: [{ value: username }, { value: password }]
+                }
+                const response = await app.submitLogin(fakeSubmitEvent)
+                expect(response).toStrictEqual({ "status": 200 })
+                // Should send failed submission
+                it("Should send error duruing creation", async () => {
+                    const fakeSubmitEvent = {
+                        preventDefault: jest.fn(),
+                        target: [{ value: "tesyMcTestface" }, { value: "test1234" }]
+                    }
+                    const response = await app.submitLogin(fakeSubmitEvent)
+                    expect(response).toStrictEqual({ "status": 422 })
+                })
+
+            })
         })
-        // Should send login data to backend
-        it("Should send login data to backend", () => {
-            const fakeSubmitEvent = {
-                preventDefault: jest.fn(),
-                target:[{value: "username"}, {value: "password"}]
-            }
-            app.submitLogin(fakeSubmitEvent)
-            expect(fetch.mock.calls[0][1]).toHaveProperty("method", "POST")
-            expect(fetch.mock.calls[0][1]).toHaveProperty("body", JSON.stringify({username: "username", password: "password"}))
-        })
-        // Should send success of submission
-        // Should send failed submission
-        // Should load modal.
+
     })
-    describe("signupHandler.js", ()=> {
-        beforeEach(()=>{
-            app = require("../js/signupHandler")
-            document.documentElement.innerHTML = html.toString();
-        })
-        afterEach(() => {
-            fetch.resetMocks();
-        })
-        // Should send signup data to backend
-        // Should send success of submission
-        // Should send failed submission
-        
-    })
-})

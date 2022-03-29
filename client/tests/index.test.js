97f2b629975b17eecdc946bc107fd59aba1c5d36
idrisimo <idrissilva@hotmail.com>
2022-03-29 10:45:51 +0100

    started writing tests added test libraries

--- client/tests/index.test.js
+++ client/tests/index.test.js
@@ -4,3 +4,41 @@ const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../../index.html'), 'utf8');
 global.fetch = require('jest-fetch-mock');
 let app;
+
+describe("Checks JS files", () => {
+    describe("loginHandler.js", () => {
+        beforeEach(()=>{
+            app = require("../js/loginHandler")
+            document.documentElement.innerHTML = html.toString();
+        })
+        afterEach(() => {
+            fetch.resetMocks();
+        })
+        // Should send login data to backend
+        it("Should send login data to backend", () => {
+            const fakeSubmitEvent = {
+                preventDefault: jest.fn(),
+                target:[{value: "username"}, {value: "password"}]
+            }
+            app.submitLogin(fakeSubmitEvent)
+            expect(fetch.mock.calls[0][1]).toHaveProperty("method", "POST")
+            expect(fetch.mock.calls[0][1]).toHaveProperty("body", JSON.stringify({username: "username", password: "password"}))
+        })
+        // Should send success of submission
+        // Should send failed submission
+        // Should load modal.
+    })
+    describe("signupHandler.js", ()=> {
+        beforeEach(()=>{
+            app = require("../js/signupHandler")
+            document.documentElement.innerHTML = html.toString();
+        })
+        afterEach(() => {
+            fetch.resetMocks();
+        })
+        // Should send signup data to backend
+        // Should send success of submission
+        // Should send failed submission
+        
+    })
+})

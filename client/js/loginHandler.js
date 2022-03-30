6e5e3c3926a9b436674c6acdc7df0cb34430cec1
Henry <uce.henrys@gmail.com>
2022-03-30 12:40:45 +0100

    created a basic login request and auth

--- client/js/loginHandler.js
+++ client/js/loginHandler.js
@@ -23,19 +23,42 @@ async function userLogIn (e) {
         const response = await fetch(url, options);
         let data = await response.json();
 
-        // if (data.err) {
-        //     throw Error(data.err);
-        // }
-        // else (
-        //     // var current = window.location.href;
-        //     // window.location.href = current.replace(/#(.*)$/, '') + '#' + id;
-        //     window.location.hash = `#${data.id}`
-        // )
-
+        if (data.err) {
+            throw Error(data.err);
+        }
+        else (
+            requestLogin(e, options)
+        )
     } catch (err) {
         alert(`Unable to Log In: ${err}`);
         console.log(`Failed to get Log In, reason: ${err}`);
     }
 }
 
+async function requestLogin(e, userData){
+    e.preventDefault();
+    options = userData;
+    try {
+        const response = await fetch(`http://localhost:3000/auth/login`, options)
+        const data = await response.json()
+        if (data.err){ throw Error(data.err); }
+        login(data);
+    } catch (err) {
+        console.warn(`Error: ${err}`);
+    }
+}
+
+function login(data){
+    localStorage.setItem('token', data.token);
+    let userInfo = jwt_decode(data.token);
+    localStorage.setItem('username', userInfo.username);
+    localStorage.setItem('email', userInfo.email);
+    window.location.href = "/dashboard.html";
+}
+
+
+function currentUser(){
+    const username = localStorage.getItem('username')
+    return username;
+}
 module.exports = {userLogIn}

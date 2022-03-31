c9cc16df5389f780c9aef0350337a83d0812e6e4
idrisimo <idrissilva@hotmail.com>
2022-03-31 08:08:44 +0100

    added button for completing a day

--- client/js/habitCardHandler.js
+++ client/js/habitCardHandler.js
@@ -143,9 +143,6 @@ function completion(event) {
             habit: habitData
         }
         
-        
-
-
         const options = {
             method: "POST",
             headers: new Headers({

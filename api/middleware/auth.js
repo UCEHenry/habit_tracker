3f546648be9753d8ba96241a92c58919c4b2aed5
idrisimo <idrissilva@hotmail.com>
2022-04-01 11:26:55 +0100

    fixed issue with auth

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -5,7 +5,7 @@ function verifyToken(req, res, next){
     const header = req.headers['authorization'];
     if (header) {
         const token = header.split(' ')[1];
-        jwt.verify(token, process.env.SECRET, async (err, data) => {
+        jwt.verify(token, 'secret', async (err, data) => {
             console.log(data);
             if(err){
                 res.status(403).json({ err: 'Invalid token' })
